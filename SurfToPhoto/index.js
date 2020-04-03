c9245d67d49ee29b6c78b438e6cf46b0da1324b4
Slynus <lustucru86@gmail.com>
2020-04-03 12:59:11 +0200

    Propal B proto 2 switchs

--- SurfToPhoto/index.js
+++ SurfToPhoto/index.js
@@ -1,44 +1,41 @@
 let grid = document.getElementById("grid-container");
 let imgHolderA = document.getElementById("img-holderA");
 let imgHolderB = document.getElementById("img-holderB");
+let imgHolderC = document.getElementById("img-holderC");
 let aboutP = document.getElementById("about-p");
 let shopP = document.getElementById("shop-p");
 let portofolioP = document.getElementById("portofolio-p");
 let socialP = document.getElementById("social-p");
 
 
-aboutP.addEventListener("mouseover", backgroundOn);
-aboutP.addEventListener("mouseout", backgroundOff);
-shopP.addEventListener("mouseover", backgroundOn2);
-shopP.addEventListener("mouseout", backgroundOff2);
+aboutP.addEventListener("mouseover", backgroundOnB);
+aboutP.addEventListener("mouseout", backgroundOffB);
+shopP.addEventListener("mouseover", backgroundOnC);
+shopP.addEventListener("mouseout", backgroundOffC);
 
 
 // TO DO event handler. Extract id from event, look up css class and change it 
 
 let bgHolderA = 1; // Default bg
 let bgHolderB = 0; // No bg
+let bgHolderC = 0; // No bg
 
-function backgroundOn() {
-    imgHolderB.classList.remove("background-2");
-    imgHolderB.classList.add("background-1");
-    imgHolderB.classList.toggle("img-holder-anim");
-    imgHolderA.classList.toggle("img-holder-anim");
-}
-
-function backgroundOff() {
-    // imgHolderB.classList.toggle("background-2");
-    imgHolderB.classList.toggle("img-holder-anim");
-    imgHolderA.classList.toggle("img-holder-anim");
-
-}
-
-function backgroundOn2() {
-    imgHolderB.classList.remove("background-1");
+function backgroundOnB() {
     imgHolderB.classList.add("background-2");
-    imgHolderB.classList.toggle("img-holder-anim");
+    imgHolderB.classList.toggle("opacitySwitch");
 }
 
-function backgroundOff2() {
+function backgroundOffB() {
     // imgHolderB.classList.toggle("background-2");
-    imgHolderB.classList.toggle("img-holder-anim");
+    imgHolderB.classList.toggle("opacitySwitch");
+}
+
+function backgroundOnC() {
+    imgHolderC.classList.add("background-3");
+    imgHolderC.classList.toggle("opacitySwitch");
+}
+
+function backgroundOffC() {
+    // imgHolderB.classList.toggle("background-2");
+    imgHolderC.classList.toggle("opacitySwitch");
 }
