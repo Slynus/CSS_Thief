e0025526e34e245373367398a7bb530b1f0148ff
Slynus <lustucru86@gmail.com>
2020-04-03 14:50:37 +0200

    Proper with many DIVs

--- SurfToPhoto/index.js
+++ SurfToPhoto/index.js
@@ -2,40 +2,30 @@ let grid = document.getElementById("grid-container");
 let imgHolderA = document.getElementById("img-holderA");
 let imgHolderB = document.getElementById("img-holderB");
 let imgHolderC = document.getElementById("img-holderC");
+let imgHolderD = document.getElementById("img-holderD");
+let imgHolderE = document.getElementById("img-holderE");
 let aboutP = document.getElementById("about-p");
 let shopP = document.getElementById("shop-p");
 let portofolioP = document.getElementById("portofolio-p");
 let socialP = document.getElementById("social-p");
 
 
-aboutP.addEventListener("mouseover", backgroundOnB);
-aboutP.addEventListener("mouseout", backgroundOffB);
-shopP.addEventListener("mouseover", backgroundOnC);
-shopP.addEventListener("mouseout", backgroundOffC);
+aboutP.addEventListener("mouseover", switchOpacity);
+aboutP.addEventListener("mouseout", switchOpacity);
+shopP.addEventListener("mouseover", switchOpacity);
+shopP.addEventListener("mouseout", switchOpacity);
+portofolioP.addEventListener("mouseover", switchOpacity);
+portofolioP.addEventListener("mouseout", switchOpacity);
+socialP.addEventListener("mouseover", switchOpacity);
+socialP.addEventListener("mouseout", switchOpacity);
 
-
-// TO DO event handler. Extract id from event, look up css class and change it 
-
-let bgHolderA = 1; // Default bg
-let bgHolderB = 0; // No bg
-let bgHolderC = 0; // No bg
-
-function backgroundOnB() {
-    imgHolderB.classList.add("background-2");
-    imgHolderB.classList.toggle("opacitySwitch");
+function switchOpacity(event) {
+    let imgHolder = document.getElementById(mapHolderP[event.target.id]);
+    imgHolder.classList.toggle("opacitySwitch");
 }
-
-function backgroundOffB() {
-    // imgHolderB.classList.toggle("background-2");
-    imgHolderB.classList.toggle("opacitySwitch");
-}
-
-function backgroundOnC() {
-    imgHolderC.classList.add("background-3");
-    imgHolderC.classList.toggle("opacitySwitch");
-}
-
-function backgroundOffC() {
-    // imgHolderB.classList.toggle("background-2");
-    imgHolderC.classList.toggle("opacitySwitch");
+let mapHolderP = {
+    "about-p": "img-holderB",
+    "shop-p": "img-holderC",
+    "portofolio-p": "img-holderD",
+    "social-p": "img-holderE"
 }
