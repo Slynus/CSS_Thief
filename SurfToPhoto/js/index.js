ceb53071081b798ccac916cac9750b3ae3620e37
Slynus <lustucru86@gmail.com>
2020-04-12 18:01:28 +0200

    Add Logotype & Cross

--- SurfToPhoto/js/index.js
+++ SurfToPhoto/js/index.js
@@ -4,6 +4,6 @@ import { start as startCanvas, stop as stopCanvas } from './canvas.js';
 import { enableBgAnim } from './backgroundAnimation.js';
 
 enableBgAnim();
-startCanvas();
+// startCanvas(); // Le canvas c'est trop relou
 
 window.stopCanvas = stopCanvas;
